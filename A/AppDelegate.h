43abcd27b2d5d2931868be9daabe58341bdf0567
weiwei <weiweiwoep@163.com>
2018-11-15 17:16:57 +0800

    init

---
+++ A/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  A
+//
+//  Created by 魏伟 on 2018/11/15.
+//  Copyright © 2018 魏伟. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ A/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  A
+//
+//  Created by 魏伟 on 2018/11/15.
+//  Copyright © 2018 魏伟. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
