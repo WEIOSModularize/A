7fb2a3c1ed37999b17066a49fedc684a94dce99f
weiwei <weiweiwoep@163.com>
2018-11-16 13:57:36 +0800

    .

---
+++ A/A.h
@@ -0,0 +1,19 @@
+//
+//  A.h
+//  A
+//
+//  Created by 魏伟 on 2018/11/16.
+//  Copyright © 2018 carlw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for A.
+FOUNDATION_EXPORT double AVersionNumber;
+
+//! Project version string for A.
+FOUNDATION_EXPORT const unsigned char AVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <A/PublicHeader.h>
+
+
